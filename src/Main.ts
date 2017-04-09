466505024ceefd0884da762f5aaf62fa32b0c88f
Jordane Jouffroy <contact@jordane.net>
2017-04-09 11:27:06 +0200

    add cli mod

--- src/Main.ts
+++ src/Main.ts
@@ -1,5 +1,6 @@
 import { client } from 'electron-connect';
 import { BrowserWindow } from 'electron';
+import { _ } from 'lodash';
 
 export default class Main {
 
@@ -8,27 +9,54 @@ export default class Main {
     static BrowserWindow;
     static viewsDirectory: string;
 
-    private static onWindowAllClosed() {
+    private static onWindowAllClosed(): void {
         if (process.platform !== 'darwin') {
             Main.application.quit();
         }
     }
 
-    private static onClose() {
+    private static onClose(): void {
         Main.mainWindow = null;
     }
 
-    private static onReady() {
-        Main.mainWindow = new Main.BrowserWindow({width: 800, height: 600})
-        Main.mainWindow.loadURL(`file://${Main.viewsDirectory}/index.html`);
-        Main.mainWindow.on('closed', Main.onClose);
+    private static onReady(): void {
+        var args = require('minimist')(process.argv);
+
+        if (Main.isCLI(args))
+            // electron . --arg="test"
+            Main.launchCLIApp();
+        else
+            Main.launchDesktopApp();
 
         if (process.env.ELECTRON_ENV === "development") {
             client.create(Main.mainWindow);
         }
     }
 
-    static main(app: Electron.App, browserWindow: typeof BrowserWindow) {
+    private static launchCLIApp(): void {
+        console.log("in CLI mod");
+        Main.application.quit();
+    }
+
+    private static launchDesktopApp(): void {
+        Main.mainWindow = new Main.BrowserWindow({
+            // titleBarStyle: 'hidden',
+            // transparent: true,
+            // frame: false,
+            width: 800,
+            height: 600,
+            minWidth: 200,
+            minHeight: 200
+        });
+        Main.mainWindow.loadURL(`file://${Main.viewsDirectory}/index.html`);
+        Main.mainWindow.on('closed', Main.onClose);
+    }
+
+    private static isCLI(args: Object): Boolean {
+        return _.has(args, "arg");
+    }
+
+    static main(app: Electron.App, browserWindow: typeof BrowserWindow): void {
         Main.viewsDirectory = `${__dirname}/../views`;
         Main.BrowserWindow = browserWindow;
         Main.application = app;
